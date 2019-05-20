6f09788046dbd134af94dde20d2598596b665845
Nebelparder <teinfinger@gmail.com>
2019-05-20 12:54:58 +0200

    addBatchClicked is working (url is not correct)

--- lagerhaus/lagerhaus/wwwroot/js/site.js
+++ lagerhaus/lagerhaus/wwwroot/js/site.js
@@ -1,21 +1,31 @@
 ﻿$(document).ready(_ => {
+    const url = 'https://f46acc4f-9fc6-448d-addb-84cee3ec619b.mock.pstmn.io';
 
-})
+    $('#btnAddBatch').on('click', event => addBatchClicked(url));
 
-function addBachClicked() {
-    $.post(url, {
-        fruit_name: inputFruitName, month: inputMonth, amount: inputAmount, storage_date: inputStorageData, region: inputRegion, ripeness: inputRipeness
-    }, function (result, status) {
-        const element = document.getElementById('txtStatus');
-        element.value = 'Successfull';
-        element.backgroundColor = 'red';
+    $.getJSON(url + '/batches/Apple/2019/6').then(data => {
+        console.log(data);
+        if (data.length === 0) {
+            console.log('Request is empty');
+        }
     });
-    return null;
-}
-
-fruit_name
-month
-amount
-storage_date
-region
-ripeness
+            
+    function addBatchClicked(url) {
+        const inputFruitName = document.getElementById('txtFruitName').value;
+        const inputMonth = document.getElementById('txtMonth').value;
+        const inputAmount = document.getElementById('txtAmount').value;
+        const inputStorageDate = document.getElementById('txtStorageDate').value;
+        const inputRegion = document.getElementById('txtRegion').value;
+        const inputRipeness = document.getElementById('txtRipeness').value;
+        console.log(inputRegion);
+        $.post(url, {
+            fruit_name: inputFruitName, month: inputMonth, amount: inputAmount, storage_date: inputStorageDate, region: inputRegion, ripeness: inputRipeness
+        }, function (result, status) {
+            const element = document.getElementById('txtStatus');
+            element.value = status;
+            element.backgroundColor = 'red';
+        });
+        console.log(inputAmount);
+        return null;
+    }
+});
