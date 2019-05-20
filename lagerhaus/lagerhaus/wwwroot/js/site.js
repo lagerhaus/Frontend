0ded82a1231652ffcc44406d88e22ffd76bbebb9
marcolehner12 <lehner681@gmail.com>
2019-05-20 12:59:40 +0200

    Fill Batches Table

--- lagerhaus/lagerhaus/wwwroot/js/site.js
+++ lagerhaus/lagerhaus/wwwroot/js/site.js
@@ -1,21 +1,51 @@
 ﻿$(document).ready(_ => {
 
-})
+    const url = 'https://f46acc4f-9fc6-448d-addb-84cee3ec619b.mock.pstmn.io';
 
-function addBachClicked() {
-    $.post(url, {
-        fruit_name: inputFruitName, month: inputMonth, amount: inputAmount, storage_date: inputStorageData, region: inputRegion, ripeness: inputRipeness
-    }, function (result, status) {
-        const element = document.getElementById('txtStatus');
-        element.value = 'Successfull';
-        element.backgroundColor = 'red';
+    $.getJSON(url + '/batches').then(data => {
+
+        console.log('fill table with data =>');
+        console.log(data);
+
+        
+
+        for (var i in data) {
+            let tr = $('<tr>');
+            $('#tblBatches').append(tr);
+
+
+            $('<th>').html(data[i].fruit_name).appendTo(tr);
+            $('<th>').html(data[i].month).appendTo(tr);
+            $('<th>').html(data[i].amount).appendTo(tr);
+            $('<th>').html(data[i].storage_date).appendTo(tr);
+            $('<th>').html(data[i].region).appendTo(tr);
+            $('<th>').html(data[i].ripeness).appendTo(tr);
+
+            
+        }
     });
-    return null;
-}
 
-fruit_name
-month
-amount
-storage_date
-region
-ripeness
+
+
+    //function addBachClicked() {
+    //    $.post(url, {
+    //        fruit_name: inputFruitName, month: inputMonth, amount: inputAmount, storage_date: inputStorageData, region: inputRegion, ripeness: inputRipeness
+    //    }, function (result, status) {
+    //        const element = document.getElementById('txtStatus');
+    //        element.value = 'Successfull';
+    //        element.backgroundColor = 'red';
+    //    });
+    //    return null;
+    //}
+
+
+});
+
+
+
+//fruit_name
+//month
+//amount
+//storage_date
+//region
+//ripeness
