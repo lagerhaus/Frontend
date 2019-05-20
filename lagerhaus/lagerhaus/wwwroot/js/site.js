7b1fdf52b0f290df141aad06b1274fdf63d6c30d
marcolehner12 <lehner681@gmail.com>
2019-05-20 11:55:36 +0200

    Site.js url added

--- lagerhaus/lagerhaus/wwwroot/js/site.js
+++ lagerhaus/lagerhaus/wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(x => {
+    console.log('JQuery ready');
+    const url = 'https://f46acc4f-9fc6-448d-addb-84cee3ec619b.mock.pstmn.io';
 
-// Write your JavaScript code.
+    $.getJSON(url + '/batches/Apple/2019/6').then(data => {
+        console.log(data);
+        if (data.length === 0) {
+            console.log('Request is empty');
+        }
+    })
+})
+
+
