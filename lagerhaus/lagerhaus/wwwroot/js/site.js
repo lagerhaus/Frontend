f25a61dd3ce4c2c0c733e01ca5a37a870ab68d65
marcolehner12 <lehner681@gmail.com>
2019-05-20 13:04:26 +0200

    Site.js updated

--- lagerhaus/lagerhaus/wwwroot/js/site.js
+++ lagerhaus/lagerhaus/wwwroot/js/site.js
@@ -1,14 +1,25 @@
 ﻿$(document).ready(_ => {
     const url = 'https://f46acc4f-9fc6-448d-addb-84cee3ec619b.mock.pstmn.io';
 
-    $('#btnAddBatch').on('click', event => addBatchClicked(url));
+    $.getJSON(url + '/batches').then(data => {
 
-    $.getJSON(url + '/batches/Apple/2019/6').then(data => {
+        console.log('fill table with data =>');
         console.log(data);
-        if (data.length === 0) {
-            console.log('Request is empty');
+
+        for (var i in data) {
+            let tr = $('<tr>');
+            $('#tblBatches').append(tr);
+
+            $('<th>').html(data[i].fruit_name).appendTo(tr);
+            $('<th>').html(data[i].month).appendTo(tr);
+            $('<th>').html(data[i].amount).appendTo(tr);
+            $('<th>').html(data[i].storage_date).appendTo(tr);
+            $('<th>').html(data[i].region).appendTo(tr);
+            $('<th>').html(data[i].ripeness).appendTo(tr);
         }
     });
+
+    $('#btnAddBatch').on('click', event => addBatchClicked(url));
             
     function addBatchClicked(url) {
         const inputFruitName = document.getElementById('txtFruitName').value;
@@ -29,52 +40,3 @@
         return null;
     }
 });
-    const url = 'https://f46acc4f-9fc6-448d-addb-84cee3ec619b.mock.pstmn.io';
-
-    $.getJSON(url + '/batches').then(data => {
-
-        console.log('fill table with data =>');
-        console.log(data);
-
-        
-
-        for (var i in data) {
-            let tr = $('<tr>');
-            $('#tblBatches').append(tr);
-
-
-            $('<th>').html(data[i].fruit_name).appendTo(tr);
-            $('<th>').html(data[i].month).appendTo(tr);
-            $('<th>').html(data[i].amount).appendTo(tr);
-            $('<th>').html(data[i].storage_date).appendTo(tr);
-            $('<th>').html(data[i].region).appendTo(tr);
-            $('<th>').html(data[i].ripeness).appendTo(tr);
-
-            
-        }
-    });
-
-
-
-    //function addBachClicked() {
-    //    $.post(url, {
-    //        fruit_name: inputFruitName, month: inputMonth, amount: inputAmount, storage_date: inputStorageData, region: inputRegion, ripeness: inputRipeness
-    //    }, function (result, status) {
-    //        const element = document.getElementById('txtStatus');
-    //        element.value = 'Successfull';
-    //        element.backgroundColor = 'red';
-    //    });
-    //    return null;
-    //}
-
-
-});
-
-
-
-//fruit_name
-//month
-//amount
-//storage_date
-//region
-//ripeness
